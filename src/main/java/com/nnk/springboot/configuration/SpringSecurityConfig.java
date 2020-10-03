3b9967fc483f0608e3a8fe61dd7ee3c29e3423f6
MrgnCpn <morgancpn.dev@gmail.com>
2020-10-03 02:35:00 +0200

    Resolve front bugs, import bootstrap in POM

--- src/main/java/com/nnk/springboot/configuration/SpringSecurityConfig.java
+++ src/main/java/com/nnk/springboot/configuration/SpringSecurityConfig.java
@@ -22,7 +22,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers("/admin*").hasRole("ADMIN")
+            .antMatchers("/user*").hasRole("ADMIN")
             .antMatchers("*").hasRole("USER")
             .anyRequest().authenticated()
             .and()
