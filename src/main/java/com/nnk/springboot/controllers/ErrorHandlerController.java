76ec43749675402c7a696e7bca4c944912f1031d
MrgnCpn <morgancpn.dev@gmail.com>
2020-10-09 13:45:17 +0200

    Error controller

---
+++ src/main/java/com/nnk/springboot/controllers/ErrorHandlerController.java
@@ -0,0 +1,23 @@
+package com.nnk.springboot.controllers;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ErrorHandlerController implements ErrorController {
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+
+    @GetMapping("/error")
+    public ModelAndView handleError() {
+        ModelAndView mav = new ModelAndView();
+        String errorMessage= "You are not authorized for the requested data.";
+        mav.addObject("errorMsg", errorMessage);
+        mav.setViewName("403");
+        return mav;
+    }
+}
